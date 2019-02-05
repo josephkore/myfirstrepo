5c9a490d16c6ecc24788c15bb5d294fcb6bd660e
Dream <josephkore24@gmail.com>
2019-02-04 21:10:32 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#calculate the sum and mean of five numbers
+#say 1,2,3,4,5
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+sum=a+b+c+d+e
+mean=sum/5
+print(sum)
+print(mean)
